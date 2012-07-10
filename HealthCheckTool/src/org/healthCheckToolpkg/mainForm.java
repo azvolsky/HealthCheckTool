f1de12a9444ee689dc2f0e2373f25b318dcdf54b
azvolsky <azvolsky@gmail.com>
2012-07-10 14:42:22 +0300

    new commit›M

--- HealthCheckTool/src/org/healthCheckToolpkg/mainForm.java
+++ HealthCheckTool/src/org/healthCheckToolpkg/mainForm.java
@@ -4,8 +4,8 @@ package org.healthCheckToolpkg;
  * Created with IntelliJ IDEA.
  * User: azvolskiy
  * Date: 7/10/12
- * Time: 2:22 PM
+ * Time: 2:28 PM
  * To change this template use File | Settings | File Templates.
  */
-public class jmxHttpInvoke {
+public class mainForm {
 }
